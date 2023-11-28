91a901e50a4bd81a2791a62d7de47ce27a2739c2
AndresElosua <aelo1993@gmail.com>
2023-11-27 18:18:48 -0600

    jwild

---
+++ SurfsUp/Starter_Code/app.py
@@ -0,0 +1,34 @@
+# Import the dependencies.
+
+from sqlalchemy import create_engine, inspect, MetaData
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
