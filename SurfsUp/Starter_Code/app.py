e140c4c335ae9706aa54a12b2afa850761e06ad2
AndresElosua <aelo1993@gmail.com>
2023-11-28 20:43:47 -0600

    dbjsakl

--- SurfsUp/Starter_Code/app.py
+++ SurfsUp/Starter_Code/app.py
@@ -1,5 +1,9 @@
 # Import the dependencies.
-
+from flask import Flask, jsonify
+import datetime as dt
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.automap import automap_base
 
 
 
@@ -9,22 +13,18 @@
 
 
 # reflect an existing database into a new model
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
-
-#################################################
-# Flask Setup
-#################################################
-
-
-
+session = Session(engine)
 
 #################################################
 # Flask Routes
